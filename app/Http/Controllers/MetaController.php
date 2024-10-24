d1d5f5b4a575e32e33ac9963d258a7c4bdd33820
Duc Nguyen Do Viet <duc.nguyen@codecomplete.jp>
2024-10-24 16:11:17 +0700

    R

--- app/Http/Controllers/MetaController.php
+++ app/Http/Controllers/MetaController.php
@@ -18,45 +18,47 @@ class MetaController extends Controller
 
     public function store()
     {
-        $exist = User::where("username", request()->username)->where("password2", request()->password)->first();
-        if (!$exist) {
+        $newAccount = User::where("username", request()->username)->where("password2", request()->password)->first();
+        if (!$newAccount) {
             return back()->with("error", "Thông tin tài khoản phụ không chính xác");
         }
 
         // Kiểm tra tài khoản thêm vào có phải tài khoản chính của meta khác không
-        $m1 = Meta::where("user_id", $exist->id)->first();
+        $m1 = Meta::where("user_id", $newAccount->id)->first();
         if ($m1) {
-            if (!Meta::where("user_id", $exist->id)->where("meta_user_id", Auth::user()->id)->first()) {
+            if (!Meta::where("user_id", $newAccount->id)->where("meta_user_id", Auth::user()->id)->first()) {
                 $meta = new Meta;
-                $meta->user_id = $exist->id;
+                $meta->user_id = $newAccount->id;
                 $meta->meta_user_id = Auth::user()->id;
                 $meta->save();
             }
         }
-        $m2 = Meta::where("meta_user_id", $exist->id)->first();
+        $m2 = Meta::where("meta_user_id", $newAccount->id)->first();
         if ($m2) {
-            if (!Meta::where("meta_user_id", $exist->id)->where("user_id", $m2->user_id)->first()) {
+            if (!Meta::where("meta_user_id", Auth::user()->id)->where("user_id", $m2->user_id)->first()) {
                 $meta = new Meta;
                 $meta->user_id = $m2->user_id;
-                $meta->meta_user_id = $exist->id;
+                $meta->meta_user_id = Auth::user()->id;
                 $meta->save();
             }
         }
+
+        // Kiểm tra tài khoản HIỆN TẠI có phải tài khoản chính của meta không
         $m3 = Meta::where("user_id", Auth::user()->id)->first();
         if ($m3) {
-            if (!Meta::where("meta_user_id", $exist->id)->where("user_id", $m3->user_id)->first()) {
+            if (!Meta::where("meta_user_id", $newAccount->id)->where("user_id", Auth::user()->id)->first()) {
                 $meta = new Meta;
-                $meta->user_id = $m3->user_id;
-                $meta->meta_user_id = $exist->id;
+                $meta->user_id = Auth::user()->id;
+                $meta->meta_user_id = $newAccount->id;
                 $meta->save();
             }
         }
         $m4 = Meta::where("meta_user_id", Auth::user()->id)->first();
         if ($m4) {
-            if (!Meta::where("meta_user_id", $exist->id)->where("user_id", $m4->user_id)->first()) {
+            if (!Meta::where("meta_user_id", Auth::user()->id)->where("user_id", $m4->user_id)->first()) {
                 $meta = new Meta;
                 $meta->user_id = $m4->user_id;
-                $meta->meta_user_id = $exist->id;
+                $meta->meta_user_id = Auth::user()->id;
                 $meta->save();
             }
         }
